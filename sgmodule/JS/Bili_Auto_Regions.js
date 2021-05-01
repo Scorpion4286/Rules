95e3f6e3e9cf9c7b54e799535e0d622400271083
Scorpion4286 <Scorpion4286@outlook.com>
2021-05-01 12:13:23 +0800

    update

--- sgmodule/JS/Bili_Auto_Regions.js
+++ sgmodule/JS/Bili_Auto_Regions.js
@@ -1,28 +1,224 @@
-const Group = $persistentStore.read('BiliArea_Policy') || 'StreamingSE'; //Your blibli policy group name.
-const CN = $persistentStore.read('BiliArea_CN') || 'DIRECT'; //Your China sub-policy name.
-const TW = $persistentStore.read('BiliArea_TW') || 'TW-Point'; //Your Taiwan sub-policy name.
-const HK = $persistentStore.read('BiliArea_HK') || 'HK-Point'; //Your HongKong sub-policy name.
+let body = JSON.parse($response.body.replace(/"\u53d7\u9650"/g, `""`));
+let $ = nobyda();
+const play = body.data || body.result || {};
+const run = SwitchRegion();
 
-var obj = JSON.parse($response.body),
-	obj = (obj.result || obj.data || {}).title || '';
-const current = $surge.selectGroupDetails().decisions[Group] || 'Policy error ⚠️'
-const str = (() => {
-	if (obj.match(/\u50c5[\u4e00-\u9fa5]+\u6e2f/)) {
-		if (current != HK) return HK;
-	} else if (obj.match(/\u50c5[\u4e00-\u9fa5]+\u53f0/)) {
-		if (current != TW) return TW;
-	} else if (current != CN) return CN;
-})()
+async function SwitchRegion() {
+	const Group = $.read('BiliArea_Policy') || 'StreamingSE'; //Your blibli policy group name.
+	const CN = $.read('BiliArea_CN') || 'DIRECT'; //Your China sub-policy name.
+	const TW = $.read('BiliArea_TW') || 'TW-Point'; //Your Taiwan sub-policy name.
+	const HK = $.read('BiliArea_HK') || 'HK-Point'; //Your HongKong sub-policy name.
+	const current = await $.getPolicy(Group) || 'Policy error ⚠️';
+	const area = (() => {
+		if (/\u50c5[\u4e00-\u9fa5]+\u6e2f/.test(play.title)) {
+			if (current != HK) return HK;
+		} else if (/\u50c5[\u4e00-\u9fa5]+\u53f0/.test(play.title)) {
+			if (current != TW) return TW;
+		} else if (current != CN) return CN;
+	})()
 
-if (str) {
-	const change = $surge.setSelectGroupPolicy(Group, str);
-	const notify = $persistentStore.read('BiliAreaNotify') === 'true';
-	if (!notify) $notification.post(obj, ``, `${current}  =>  ${str}  =>  ${change?`🟢`:`🔴`}`);
-	if (change) {
-		$done(); //Kill the connection. Due to the characteristics of Surge, it will auto reconnect with the new policy.
+	if (area) {
+		const change = await $.setPolicy(Group, area);
+		const notify = $.read('BiliAreaNotify') === 'true';
+		if (!notify) $.notify(play.title || ``, ``, `${current}  =>  ${area}  =>  ${change?`🟢`:`🔴`}`);
+		if (change && !$.isQuanX) {
+			$done(); //Kill the connection. Due to the characteristics of Surge, it will auto reconnect with the new policy.
+		} else {
+			QueryRating();
+		}
 	} else {
-		$done({});
+		QueryRating();
 	}
-} else {
-	$done({});
+}
+
+async function QueryRating() {
+	try {
+		const ratingEnabled = $.read('BiliDoubanRating') === 'false';
+		if (!ratingEnabled && play.title && body.data && body.data.badge_info) {
+			const [t1, t2] = await Promise.all([
+				GetRawInfo(play.title),
+				GetRawInfo(play.origin_name)
+			]);
+			const exYear = body.data.publish.release_date_show.split(/^(\d{4})/)[1];
+			const filterInfo = [play.title, play.origin_name, play.staff.info + play.actor.info, exYear];
+			const [rating, folk, name, id, other] = ExtractMovieInfo([...t1, ...t2], filterInfo);
+			body.data.detail = body.data.new_ep.desc.replace(/连载中,/, '');
+			body.data.badge_info.text = `⭐️ 豆瓣：${!$.is403?`${rating||'无评'}分 (${folk||'无评价'})`:`查询频繁！`}`;
+			body.data.evaluate = `${body.data.evaluate||''}\n\n豆瓣评分搜索结果: ${JSON.stringify(other,0,1)}`;
+			body.data.new_ep.desc = name;
+			body.data.styles.unshift({
+				name: "⭐️ 点击此处打开豆瓣剧集详情页",
+				url: `https://m.douban.com/${id?`movie/subject/${id}/`:`search/?query=${encodeURI(play.title)}`}`
+			});
+		}
+	} catch (err) {
+		console.log(`Douban rating: \n${err}\n`);
+	} finally {
+		$done({
+			body: JSON.stringify(body)
+		});
+	}
+}
+
+function ExtractMovieInfo(ret, fv) {
+	const sole = new Set(ret.map(s => JSON.stringify(s))); //delete duplicate
+	const f1 = [...sole].map(p => JSON.parse(p))
+		.filter(t => {
+			t.accuracy = 0;
+			if (t.name && fv[0]) { //title
+				if (t.name.includes(fv[0].slice(0, 4))) t.accuracy++;
+				if (t.name.includes(fv[0].slice(-3))) t.accuracy++;
+			}
+			if (t.origin && fv[1]) { //origin title
+				if (t.origin.includes(fv[1].slice(0, 4))) t.accuracy++;
+				if (t.origin.includes(fv[1].slice(-3))) t.accuracy++;
+			}
+			if (t.pd && fv[2]) { //producer or actor
+				const len = t.pd.split('/').filter(c => fv[2].includes(c));
+				t.accuracy += len.length;
+			}
+			if (t.year && fv[3] && t.year == fv[3]) t.accuracy++; //year
+			return Boolean(t.accuracy);
+		});
+	let x = {}; //assign most similar
+	const f2 = f1.reduce((p, c) => c.accuracy > p ? (x = c, c.accuracy) : p, 0);
+	return [x.rating, x.folk, x.name, x.id, f1];
+}
+
+function GetRawInfo(t) {
+	let res = [];
+	let st = Date.now();
+	return new Promise((resolve) => {
+		if (!t) return resolve(res);
+		$.get({
+			url: `https://www.douban.com/search?cat=1002&q=${encodeURIComponent(t)}`,
+			headers: {
+				'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15',
+				'Cookie': JSON.stringify(st)
+			}
+		}, (error, resp, data) => {
+			if (error) {
+				console.log(`Douban rating: \n${t}\nRequest error: ${error}\n`);
+			} else {
+				if (/\u767b\u5f55<\/a>\u540e\u91cd\u8bd5\u3002/.test(data)) $.is403 = true;
+				let s = data.replace(/\n| |&#\d{2}/g, '')
+					.match(/\[\u7535\u5f71\].+?subject-cast\">.+?<\/span>/g) || [];
+				for (let i = 0; i < s.length; i++) {
+					res.push({
+						name: s[i].split(/\}\)">(.+?)<\/a>/)[1],
+						origin: s[i].split(/\u540d:(.+?)(\/|<)/)[1],
+						pd: s[i].split(/\u539f\u540d.+?\/(.+?)\/\d+<\/span>$/)[1],
+						rating: s[i].split(/">(\d\.\d)</)[1],
+						folk: s[i].split(/(\d+\u4eba\u8bc4\u4ef7)/)[1],
+						id: s[i].split(/sid:(\d+)/)[1],
+						year: s[i].split(/(\d+)<\/span>$/)[1]
+					})
+				}
+				let et = ((Date.now() - st) / 1000).toFixed(2);
+				console.log(`Douban rating: \n${t}\n${res.length} movie info searched. (${et} s)\n`);
+			}
+			resolve(res);
+		})
+	})
+}
+
+function nobyda() {
+	const isHTTP = typeof $httpClient != "undefined";
+	const isLoon = typeof $loon != "undefined";
+	const isQuanX = typeof $task != "undefined";
+	const isSurge = typeof $network != "undefined" && typeof $script != "undefined";
+	const notify = (title, subtitle, message) => {
+		console.log(`${title}\n${subtitle}\n${message}`);
+		if (isQuanX) $notify(title, subtitle, message);
+		if (isHTTP) $notification.post(title, subtitle, message);
+	}
+	const read = (key) => {
+		if (isQuanX) return $prefs.valueForKey(key);
+		if (isHTTP) return $persistentStore.read(key);
+	}
+	const adapterStatus = (response) => {
+		if (!response) return null;
+		if (response.status) {
+			response["statusCode"] = response.status;
+		} else if (response.statusCode) {
+			response["status"] = response.statusCode;
+		}
+		return response;
+	}
+	const getPolicy = (groupName) => {
+		const m = `切换策略失败, 您的版本不支持该功能\n`
+		if (isSurge) {
+			if (typeof($httpAPI) === 'undefined') return m;
+			return new Promise((resolve) => {
+				$httpAPI("GET", "v1/policy_groups/select", {
+					group_name: encodeURIComponent(groupName)
+				}, (b) => resolve(b.policy))
+			})
+		}
+		if (isLoon) {
+			if (typeof($config.getPolicy) === 'undefined') return m;
+			const getName = $config.getPolicy(groupName);
+			return getName;
+		}
+		if (isQuanX) {
+			if (typeof($configuration) === 'undefined') return m;
+			return new Promise((resolve) => {
+				$configuration.sendMessage({
+					action: "get_policy_state"
+				}).then(b => {
+					if (b.ret && b.ret[groupName]) {
+						resolve(b.ret[groupName][1]);
+					} else resolve();
+				}, () => resolve());
+			})
+		}
+	}
+	const setPolicy = (group, policy) => {
+		if (isSurge && typeof($httpAPI) !== 'undefined') {
+			return new Promise((resolve) => {
+				$httpAPI("POST", "v1/policy_groups/select", {
+					group_name: group,
+					policy: policy
+				}, (b) => resolve(!b.error))
+			})
+		}
+		if (isLoon && typeof($config.getPolicy) !== 'undefined') {
+			const set = $config.setSelectPolicy(group, policy);
+			return set;
+		}
+		if (isQuanX && typeof($configuration) !== 'undefined') {
+			return new Promise((resolve) => {
+				$configuration.sendMessage({
+					action: "set_policy_state",
+					content: {
+						[group]: policy
+					}
+				}).then((b) => resolve(!b.error), () => resolve());
+			})
+		}
+	}
+	const get = (options, callback) => {
+		if (isQuanX) {
+			options["method"] = "GET";
+			$task.fetch(options).then(response => {
+				callback(null, adapterStatus(response), response.body)
+			}, reason => callback(reason.error, null, null))
+		}
+		if (isHTTP) {
+			if (isSurge) options.headers['X-Surge-Skip-Scripting'] = false;
+			$httpClient.get(options, (error, response, body) => {
+				callback(error, adapterStatus(response), body)
+			})
+		}
+	}
+	return {
+		getPolicy,
+		setPolicy,
+		isSurge,
+		isQuanX,
+		isLoon,
+		notify,
+		read,
+		get
+	}
 }
